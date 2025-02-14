5351990ddcaaaa6bcd74fcf029c9bca4a3b6a1fb
ThatSafeSpace <denis.orekhov@yahoo.de>
2025-02-14 20:18:50 +0100

    [INIT] XHR exercise

--- extra-addons/debugger/static/src/webclient/components/webClientPanelComponent.js
+++ extra-addons/debugger/static/src/webclient/components/webClientPanelComponent.js
@@ -5,16 +5,16 @@ export class WebClientPanelComponent extends Component {
     static template = 'debugger.WebClientPanelComponent';
     static props = {}
     setup() {
-		onWillStart(async () => {
-		    const appHeaders = new Headers();
-		    appHeaders.set("Sec-Fetch-Dest", "document");
-		    appHeaders.append("Sec-Fetch-Mode", "navigate");
-		    appHeaders.append( "Sec-Fetch-Site", 'none');
-		    appHeaders.append("Sec-Fetch-User", "?1");
-		    this.response = await fetch('http://localhost:8069/odoo/sales/7?debug=1', {
-		        headers: appHeaders
+        onWillStart(async () => {
+            const appHeaders = new Headers();
+            appHeaders.set("Sec-Fetch-Dest", "document");
+            appHeaders.append("Sec-Fetch-Mode", "navigate");
+            appHeaders.append( "Sec-Fetch-Site", 'none');
+            appHeaders.append("Sec-Fetch-User", "?1");
+            this.response = await fetch('http://localhost:8069/odoo/sales/7?debug=1', {
+                headers: appHeaders
             }).then(function(response) {
-              return (response.text());
+                return (response.text());
             });
             console.log(this.response)
         });
