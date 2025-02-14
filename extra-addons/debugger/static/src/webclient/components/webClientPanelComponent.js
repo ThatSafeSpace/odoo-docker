a6387607df1c06c647cc40c37ab854042e7f1a87
ThatSafeSpace <denis.orekhov@yahoo.de>
2025-02-14 20:15:42 +0100

    [INIT] XHR exercise

--- extra-addons/debugger/static/src/webclient/components/webClientPanelComponent.js
+++ extra-addons/debugger/static/src/webclient/components/webClientPanelComponent.js
@@ -1,7 +1,22 @@
 /** @odoo-module **/
-import { Component } from "@odoo/owl";
+import { Component, onWillStart } from "@odoo/owl";
 
 export class WebClientPanelComponent extends Component {
     static template = 'debugger.WebClientPanelComponent';
     static props = {}
+    setup() {
+		onWillStart(async () => {
+		    const appHeaders = new Headers();
+		    appHeaders.set("Sec-Fetch-Dest", "document");
+		    appHeaders.append("Sec-Fetch-Mode", "navigate");
+		    appHeaders.append( "Sec-Fetch-Site", 'none');
+		    appHeaders.append("Sec-Fetch-User", "?1");
+		    this.response = await fetch('"http://localhost:8069/odoo/sales/7?debug=1"', {
+		        headers: appHeaders
+            }).then(function(response) {
+              return (response.text());
+            });
+            console.log(this.response)
+        });
+    }
 }
